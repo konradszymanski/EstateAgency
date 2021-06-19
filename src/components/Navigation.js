56cfc9a801e63e6dd0acac310b7d31c0a0ae53bb
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-06-19 09:46:52 +0100

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,7 @@ const Navigation = () => {
     return (
         <nav>
             <ul>
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
+                <li>Estate Agency</li>
             </ul>
         </nav>
     )
