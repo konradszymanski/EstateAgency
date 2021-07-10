ec0aca1a410abf2bcfdb33c6a11980aa2b7e305d
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-07-10 11:47:56 +0100

    heroku update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import './styles/App.css';
 import './styles/navigation.css'
 import './styles/tile.css'
 import Tile from './components/Tile';
-import Filter from './components/Filter';
+// import Filter from './components/Filter';
 
 import React, { useEffect, useState } from "react";
 import Navigation from './components/Navigation';
-
-//  let houses=[]
-// houses.push({price:27500,area:"Handsworth",type:"Flat",image:`https://media.rightmove.co.uk/dir/crop/10:9-16:9/108k/107051/78903606/107051_RS0730_IMG_11_0000_max_476x317.jpeg`})
-// houses.push({price:1450000,area:"Harbourne",type:"House",image:`https://media.rightmove.co.uk/dir/crop/10:9-16:9/93k/92029/104484854/92029_581009_IMG_00_0000_max_476x317.jpeg`})
-// houses.push({price:165000,area:"Edgbaston",type:"Maisonette",image:`https://media.rightmove.co.uk/dir/crop/10:9-16:9/73k/72455/97846952/72455_107VC_IMG_00_0000_max_476x317.jpg`})
-
 
 function App() {
   
@@ -32,7 +26,7 @@ const [houses, fillHouses] = useState([]);
   
     <div className="App">
       <Navigation />
-      <Filter />
+      {/* <Filter /> */}
       <header className="App-header">
       {houses.map(
         h =>
